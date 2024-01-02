2f355bfe30fe29fe8f9e5c24e23b96e43429ff90
Nishalya Pethiyagoda <pethiyagodank@gmail.com>
2024-01-02 21:59:57 +0530

    sudoku 9 by 9 characters identified and written to txt file.

--- algo.py
+++ algo.py
@@ -1,18 +1,18 @@
 
+
 import cv2
 import numpy as np
-import imutils
 from tensorflow.keras.models import load_model
+import imutils
 
 classes = np.arange(0, 10)
 
 model = load_model('model-OCR.h5')
-
 # print(model.summary())
 input_size = 48
 
 
-def get_perspective(img, location, height = 900, width = 900):
+def get_perspective(img, location, height=900, width=900):
     """Takes an image and location os interested region.
         And return the only the selected region with a perspective transformation"""
     pts1 = np.float32([location[0], location[3], location[1], location[2]])
@@ -23,7 +23,8 @@ def get_perspective(img, location, height = 900, width = 900):
     result = cv2.warpPerspective(img, matrix, (width, height))
     return result
 
-def get_InvPerspective(img, masked_num, location, height = 900, width = 900):
+
+def get_InvPerspective(img, masked_num, location, height=900, width=900):
     """Takes original image as input"""
     pts1 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
     pts2 = np.float32([location[0], location[3], location[1], location[2]])
@@ -40,7 +41,7 @@ def find_board(img):
     bfilter = cv2.bilateralFilter(gray, 13, 20, 20)
     edged = cv2.Canny(bfilter, 30, 180)
     keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours  = imutils.grab_contours(keypoints)
+    contours = imutils.grab_contours(keypoints)
 
     newimg = cv2.drawContours(img.copy(), contours, -1, (0, 255, 0), 3)
     # cv2.imshow("Contour", newimg)
@@ -57,30 +58,32 @@ def find_board(img):
     result = get_perspective(img, location)
     return result, location
 
+
 # split the board into 81 individual images
 def split_boxes(board):
     """Takes a sudoku board and split it into 81 cells.
         each cell contains an element of that board either given or an empty cell."""
-    rows = np.vsplit(board,9)
+    rows = np.vsplit(board, 9)
     boxes = []
     for r in rows:
-        cols = np.hsplit(r,9)
+        cols = np.hsplit(r, 9)
         for box in cols:
-            box = cv2.resize(box, (input_size, input_size))/255.0
+            box = cv2.resize(box, (input_size, input_size)) / 255.0
             # cv2.imshow("Splitted block", box)
             # cv2.waitKey(50)
             boxes.append(box)
-    cv2.destroyAllWindows()
     return boxes
+
 
 def displayNumbers(img, numbers, color=(0, 255, 0)):
     """Displays 81 numbers in an image or mask at the same position of each cell of the board"""
-    W = int(img.shape[1]/9)
-    H = int(img.shape[0]/9)
-    for i in range (9):
-        for j in range (9):
-            if numbers[(j*9)+i] !=0:
-                cv2.putText(img, str(numbers[(j*9)+i]), (i*W+int(W/2)-int((W/4)), int((j+0.7)*H)), cv2.FONT_HERSHEY_COMPLEX, 2, color, 2, cv2.LINE_AA)
+    W = int(img.shape[1] / 9)
+    H = int(img.shape[0] / 9)
+    for i in range(9):
+        for j in range(9):
+            if numbers[(j * 9) + i] != 0:
+                cv2.putText(img, str(numbers[(j * 9) + i]), (i * W + int(W / 2) - int((W / 4)), int((j + 0.7) * H)),
+                            cv2.FONT_HERSHEY_COMPLEX, 2, color, 2, cv2.LINE_AA)
     return img
 
 
@@ -92,7 +95,6 @@ board, location = find_board(img)
 
 gray = cv2.cvtColor(board, cv2.COLOR_BGR2GRAY)
 # print(gray.shape)
-
 rois = split_boxes(gray)
 rois = np.array(rois).reshape(-1, input_size, input_size, 1)
 
@@ -103,7 +105,7 @@ prediction = model.predict(rois)
 predicted_numbers = []
 # get classes from prediction
 for i in prediction:
-    index = (np.argmax(i)) # returns the index of the maximum number of the array
+    index = (np.argmax(i))  # returns the index of the maximum number of the array
     predicted_number = classes[index]
     predicted_numbers.append(predicted_number)
 
@@ -112,10 +114,14 @@ for i in prediction:
 # reshape the list
 board_num = np.array(predicted_numbers).astype('uint8').reshape(9, 9)
 
-for row in board_num:
-    for value in row:
-        print(value)
-cv2.imshow("Input image", img)
+# Save the Sudoku board to a text file
+with open("sudoku_board.txt", "w") as file:
+    for row in board_num:
+        row_str = " ".join(map(str, row))
+        file.write(row_str + "\n")
+
+print(board_num)
+# cv2.imshow("Input image", img)
 # cv2.imshow("Board", board)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
